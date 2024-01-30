5ba7e84ea6a180623dfe6d47c977bb10e4d7ac01
unknown <martinjoachim.hetz@dkfz.de>
2024-01-30 12:53:10 +0100

    Added browsing through image list

--- visualization/visualizer.py
+++ visualization/visualizer.py
@@ -1,9 +1,9 @@
 import sys
 import os
-
+import glob
 import numpy as np
 import matplotlib.pyplot as plt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QSlider, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QSlider, QLabel, QVBoxLayout, QWidget, QPushButton
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from PyQt5.QtCore import Qt
 
@@ -11,11 +11,12 @@ from hsi_processor import HSIProcessor
 
 
 class HSIViewer(QMainWindow):
-    def __init__(self, image, wavelengths):
+    def __init__(self, images, wavelengths):
         super().__init__()
-        self.image = image
+        self.images = images  # List of hyperspectral images
         self.wavelengths = wavelengths
-        self.last_clicked_point = None  # Variable to store the last clicked point
+        self.current_image_index = 0  # Index of the current image being viewed
+        self.last_clicked_point = None
         self.initUI()
 
     def initUI(self):
@@ -31,7 +32,7 @@ class HSIViewer(QMainWindow):
         self.slider = QSlider(self)
         self.slider.setOrientation(Qt.Horizontal)
         self.slider.setMinimum(0)
-        self.slider.setMaximum(self.image.shape[2] - 1)
+        self.slider.setMaximum(self.images[0].shape[2] - 1)
         self.slider.setValue(0)
         self.slider.valueChanged[int].connect(self.changeValue)
 
@@ -49,6 +50,16 @@ class HSIViewer(QMainWindow):
         layout.addWidget(self.label)
         layout.addWidget(self.coord_label)
         layout.addWidget(self.slider)
+
+        # Buttons for navigating images
+        self.prev_button = QPushButton('Previous Image', self)
+        self.prev_button.clicked.connect(self.prev_image)
+        self.next_button = QPushButton('Next Image', self)
+        self.next_button.clicked.connect(self.next_image)
+
+        # Update layout
+        layout.addWidget(self.prev_button)
+        layout.addWidget(self.next_button)
 
         container = QWidget()
         container.setLayout(layout)
@@ -68,15 +79,16 @@ class HSIViewer(QMainWindow):
             self.update_plot(self.slider.value())  # Update the plot with the new point
 
     def update_plot(self, channel):
+        current_image = self.images[self.current_image_index]
         self.ax.clear()
-        self.ax.imshow(self.image[:, :, channel], cmap='gray')
+        self.ax.imshow(current_image[:, :, channel], cmap='gray')
         if self.last_clicked_point:
             x, y = self.last_clicked_point
             self.ax.add_patch(plt.Rectangle((x - 0.5, y - 0.5), 1, 1, edgecolor='red', facecolor='none'))
 
             # Plotting the spectrum
             self.spectrum_ax.clear()
-            self.spectrum_ax.plot(self.wavelengths, self.image[y, x, :])
+            self.spectrum_ax.plot(self.wavelengths, current_image[y, x, :])
             self.spectrum_ax.set_title('Spectrum at '+ f'{y,x}')
             self.spectrum_ax.set_xlabel('Wavelength (nm)')
             self.spectrum_ax.set_ylabel('Intensity')
@@ -84,7 +96,7 @@ class HSIViewer(QMainWindow):
             # Marking the selected wavelength on the spectrum
             selected_wavelength = self.wavelengths[channel]
             self.spectrum_ax.axvline(x=selected_wavelength, color='red', linestyle='--')
-            self.spectrum_ax.text(selected_wavelength, max(self.image[y, x, :]), f'{selected_wavelength} nm', color='red')
+            self.spectrum_ax.text(selected_wavelength, max(current_image[y, x, :]), f'{selected_wavelength} nm', color='red')
 
         self.ax.set_title(f'Peak wavelength {self.wavelengths[channel]} nm')
         self.canvas.draw()
@@ -92,18 +104,62 @@ class HSIViewer(QMainWindow):
     def get_coordinates(self):
         return self.last_clicked_point
 
+    def changeImage(self, index):
+        self.current_image_index = index
+        self.update_plot(self.slider.value())
+
+    def prev_image(self):
+        if self.current_image_index > 0:
+            self.changeImage(self.current_image_index - 1)
+
+    def next_image(self):
+        if self.current_image_index < len(self.images) - 1:
+            self.changeImage(self.current_image_index + 1)
+
+
+def group_cu3s_files(base_dir):
+    file_dict = {}
+
+    # Walk through the directory
+    for root, dirs, files in os.walk(base_dir):
+        # Skip the root directory itself
+        if root == base_dir:
+            continue
+
+        # Extract patient name and body part from the path
+        parts = root.split(os.sep)
+        if len(parts) >= 3:
+            patient_name = parts[-2]
+            body_part = parts[-1]
+
+            # Find all .cu3s files in the current directory
+            cu3s_files = glob.glob(os.path.join(root, '*.cu3s'))
+
+            # Add to dictionary
+            if cu3s_files:
+                file_dict.setdefault(patient_name, {}).setdefault(body_part, []).extend(cu3s_files)
+
+    return file_dict
+
+
 if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.realpath(__file__))
-
+    image_dir = "C:\\Users\\C140_Martin\\Desktop\\hsi_test_data\\small"
     torso_lesion = "C:\\Users\\C140_Martin\\Downloads\\2023_11_21_10-37-01\\p4\\stomach\\Auto_006_3284.cu3s"
     mesu_path = torso_lesion
     processor = HSIProcessor()
-
-    measurement, session = processor.load_measurement(mesu_path)
-
-    hsi = processor.measurement_2_arr(measurement)
     wavelengths = processor.get_wavelengths()
+
+    hsi_dict = group_cu3s_files(image_dir)
+    images= []
+    for patient_id, patient_dict in hsi_dict.items():
+        for body_part, image_list in patient_dict.items():
+            for image in image_list:
+                measurement, session = processor.load_measurement(image)
+                hsi = processor.measurement_2_arr(measurement)
+                images.append(hsi)
+
     app = QApplication(sys.argv)
-    ex = HSIViewer(hsi, wavelengths)
+    ex = HSIViewer(images, wavelengths)
     ex.show()
     sys.exit(app.exec_())
