003a23cc67798510d6be98886598db7604b1a407
unknown <martinjoachim.hetz@dkfz.de>
2024-01-31 08:56:04 +0100

    Annotation loading added

--- hsi_analysis.py
+++ hsi_analysis.py
@@ -11,9 +11,34 @@ from visualization.visualizer import HSIViewer
 from hsi_processor import HSIProcessor
 
 
+
+def read_annotation(file_path):
+
+    file = open(file_path, 'r')
+    content = file.readlines()
+
+    return content
+
+def map_annotations_to_images(hsi_dict, annotation_dir):
+    pass
+
+def annotations_to_csv(annotation_dir):
+    annotations = []
+
+    for patient in os.listdir(annotation_dir):
+        for localization in os.listdir(os.path.join(annotation_dir,patient)):
+            for image in os.listdir(os.path.join(annotation_dir, patient, localization)):
+                for file in os.listdir(os.path.join(annotation_dir, patient, localization, image)):
+
+                    content = read_annotation(os.path.join(annotation_dir, patient, localization, image, file))
+                    print(image)
+
+
+
 def save_json(dict, path):
     with open(path, 'w') as json_file:
         json.dump(dict, json_file)
+
 
 def group_cu3s_files(base_dir):
     file_dict = {}
@@ -44,10 +69,12 @@ if __name__ == "__main__":
     image_dir = "C:\\Users\\C140_Martin\\Desktop\\hsi_test_data\\images"
     annotation_dir = "C:\\Users\\C140_Martin\\Desktop\\hsi_test_data\\annotations"
 
+    hsi_dict = group_cu3s_files(image_dir)
+    annotations_to_csv(annotation_dir)
+
     # TODO: Create function to map annotations and images
     # TODO: Function to parse annotation files
     # TODO: Create function to aggregate by patient, localization.
     # TODO: UMAP plots over locations, patients, lesions
-
 
     print("fin")
